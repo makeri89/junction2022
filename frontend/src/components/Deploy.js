2c961f7a586e7b49df18d302757ded4cd115988e
asianomainen <oskari.nuottonen@gmail.com>
2022-11-05 18:56:17 +0200

    Front spaghetti-O's

--- frontend/src/components/Deploy.js
+++ frontend/src/components/Deploy.js
@@ -1,17 +1,16 @@
-import React from "react";
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Spinner from 'react-bootstrap/Spinner';
 
-import { ProgressBar, Step } from "react-step-progress-bar";
-import styled from 'styled-components';
+import { CardActionArea } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { ProgressBar, Step } from 'react-step-progress-bar';
+import styled from 'styled-components';
 
 const Button = styled.button`
-  background-color: #8D72E1;
+  background-color: #8d72e1;
   color: white;
   font-size: 20px;
   padding: 10px 60px;
@@ -20,17 +19,43 @@ const Button = styled.button`
   cursor: pointer;
 `;
 
-const Deploy = ({ deploySmartContracts, progress }) => {
+const Deploy = () => {
   const [isDeploying, setIsDeploying] = useState(false);
+  const [isDeployed, setIsDeployed] = useState(false);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    if (isDeploying) {
+      const timeout = setTimeout(() => {
+        setProgress((progress) => progress + 1);
+      }, 100);
+      if (progress === 100) {
+        setIsDeploying(false);
+        setIsDeployed(true);
+        clearTimeout(timeout);
+      }
+    }
+  }, [isDeploying, progress]);
 
   const handleDeploy = () => {
-    deploySmartContracts();
-    setIsDeploying(true)
-  }
+    setIsDeploying(true);
+  };
 
- 
+  const Delay = ({ children, waitBeforeShown }) => {
+    const [isShown, setIsShown] = useState(false);
+
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        setIsShown(true);
+      }, waitBeforeShown);
+      return () => clearTimeout(timer);
+    }, [waitBeforeShown]);
+
+    return isShown ? children : null;
+  };
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Initializing Galaxy's Basic Income</h1>
       <br />
       <br />
@@ -40,120 +65,140 @@ const Deploy = ({ deploySmartContracts, progress }) => {
           percent={progress}
           //width={800}
           height={20}
-          filledBackground="#2E8B57"
+          filledBackground='#2E8B57'
         >
-          <Step transition="scale">
+          <Step transition='scale'>
             {({ accomplished }) => (
               <div
                 style={{
                   width: 30,
                   height: 30,
                   borderRadius: 25,
-                  background: "#8D72E1"
+                  background: '#8D72E1',
                 }}
               ></div>
             )}
           </Step>
-          <Step transition="scale">
+          <Step transition='scale'>
             {({ accomplished }) => (
               <div>
                 <img
-                  alt="img"
+                  alt='img'
                   style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
-                  width="60"
-                  src="/barTokenBg.png"
+                  width='60'
+                  src='/barTokenBg.png'
                 />
               </div>
             )}
           </Step>
-          <Step transition="scale">
+          <Step transition='scale'>
             {({ accomplished }) => (
-              <img
-                alt="img"
-                style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
-                width="60"
-                src="/enviromentTokBg.png"
-              />
+              <div>
+                <img
+                  alt='img'
+                  style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+                  width='60'
+                  src='/enviromentTokBg.png'
+                />
+              </div>
             )}
           </Step>
-          <Step transition="scale">
+          <Step transition='scale'>
             {({ accomplished }) => (
-              <img
-                alt="img"
-                style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
-                width="60"
-                src="/cultureTokBg.png"
-              />
+              <div>
+                <img
+                  alt='img'
+                  style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+                  width='60'
+                  src='/cultureTokBg.png'
+                />
+              </div>
             )}
           </Step>
-          <Step transition="scale">
+          <Step transition='scale'>
             {({ accomplished }) => (
-              <img
-                alt="img"
-                style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
-                width="80"
-                src="/foodTokenBg.png"
-              />
+              <div>
+                <img
+                  alt='img'
+                  style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+                  width='80'
+                  src='/foodTokenBg.png'
+                />
+              </div>
             )}
           </Step>
         </ProgressBar>
         <br />
         <br />
-        {!isDeploying ?
-          <Button onClick={() => handleDeploy()}>
-            Deploy Smart Contracts
-          </Button>
-          :
+
+        {progress === 0 ? (
+          <Button onClick={() => handleDeploy()}>Deploy Smart Contracts</Button>
+        ) : 100 > progress > 0 ? (
           <Button>
-            Deploying... <Spinner animation="border" />
+            Deploying... <Spinner animation='border' size='sm' />
           </Button>
-        }
-        <div>
-            <h3>All set! We are ready to go!</h3>
-            <h4>As AI Work-ethics has ruled on the year 2200,<br />we need to provide two AI for the project</h4>
+        ) : progress === 100 ? (
+          <Button>Deployed!</Button>
+        ) : null}
 
-        <div class="container" style={{display: 'flex' }}>
+        {isDeployed && (
+          <Delay waitBeforeShown={500}>
+            <div>
+              <Delay waitBeforeShown={1000}>
+                <h3>All set! We are ready to go!</h3>
+              </Delay>
 
-          <Card sx={{ maxWidth: 245 }} style={{marginRight: '2em'}}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                image="/gpt2.png"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  GPT-2
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Product Owner
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
+              <Delay waitBeforeShown={1500}>
+                <h4>
+                  As AI Work-ethics has ruled on the year 2200,
+                  <br />
+                  we need to provide two AI for the project
+                </h4>
+              </Delay>
 
-          <Card sx={{ maxWidth: 245 }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                image="/gpt3.png"
-                alt="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  GPT-3
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Scrum Master
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </div>
-        </div>
+              <div class='container' style={{ display: 'flex' }}>
+                <Delay waitBeforeShown={2000}>
+                  <Card sx={{ maxWidth: 245 }} style={{ marginRight: '2em' }}>
+                    <CardActionArea>
+                      <CardMedia component='img' image='/gpt2.png' />
+                      <CardContent>
+                        <Typography gutterBottom variant='h5' component='div'>
+                          GPT-2
+                        </Typography>
+                        <Typography variant='body2' color='text.secondary'>
+                          Product Owner
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Delay>
+
+                <Delay waitBeforeShown={2500}>
+                  <Card sx={{ maxWidth: 245 }}>
+                    <CardActionArea>
+                      <CardMedia
+                        component='img'
+                        image='/gpt3.png'
+                        alt='green iguana'
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant='h5' component='div'>
+                          GPT-3
+                        </Typography>
+                        <Typography variant='body2' color='text.secondary'>
+                          Scrum Master
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Delay>
+              </div>
+            </div>
+          </Delay>
+        )}
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Deploy
+export default Deploy;
