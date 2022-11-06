7a4aff63212a13beee7f839219a1b156c0409bbd
Markus Kaihola <markus.kaihola@helsinki.fi>
2022-11-06 05:35:36 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -9,9 +9,7 @@ from walletStorageAbi import walletStorageAbi
 
 from os.path import join, dirname
 from dotenv import load_dotenv
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+load_dotenv()
 
 app = FastAPI()
 
